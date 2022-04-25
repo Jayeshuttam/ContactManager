0e313bf31effcd9e10833679e2b8f6c6dbefc14e
Jayeshuttam <jayeshuttam7844@gmail.com>
2022-04-25 16:26:29 -0300

    second commit

--- contactmanager/src/components/Contacts/AddContact/AddContact.jsx
+++ contactmanager/src/components/Contacts/AddContact/AddContact.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 let AddContact =()=>{
     return(
         <React.Fragment>
-            <h2>Add List</h2>
+            <section className='add-contact p-3'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <p className='h3 text-success f2-bold'>Create Contact</p>
+                            <p className='fst-italic'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere dolorum et iusto incidunt repudiandae nesciunt tenetur asperiores nostrum aliquam alias voluptas repellendus sunt at numquam porro blanditiis error, dignissimos necessitatibus.</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="com-md-4">
+                            <form>
+                                <div className="mb-2">
+                                    <input type="text" className='form-control' placeholder='Name'/>
+                                </div>
+                                <div className="mb-2">
+                                    <input type="text" className='form-control' placeholder='Photo Url'/>
+                                </div>
+                                <div className="mb-2">
+                                    <input type="text" className='form-control' placeholder='Email'/>
+                                </div>
+                                <div className="mb-2">
+                                    <input type="text" className='form-control' placeholder='Company'/>
+                                </div>
+                                <div className="mb-2">
+                                    <input type="text" className='form-control' placeholder='Title'/>
+                                </div>
+                                <div className="mb-2">
+                                    <select className='form-control'>
+                                        <option value="">Select a Group</option>
+                                    </select>
+                                </div>
+                                <div className="mb-2">
+                                    <input type="submit" className='btn btn-success' value='Create'/>
+                                    <Link to={'/contacts/list'} className='btn btn-danger ms-2'>Cancel</Link>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </React.Fragment>
     )
 }
--- contactmanager/src/components/Contacts/ViewContact/ViewContact.jsx
+++ contactmanager/src/components/Contacts/ViewContact/ViewContact.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
-
+import  {Link} from 'react-router-dom'
 let ViewContact =()=>{
     return(
         <React.Fragment>
-            <h2>View Contact</h2>
+            <section className='view-contact p-3'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <p className='h3 text-warning fw-bold'>View Contact</p>
+                            <p className='fst-italic'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem non molestiae quis et quisquam quia delectus cupiditate illum, incidunt sequi quos in facere ab voluptate minima adipisci eum quidem possimus.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='view-contact mt-3'>
+                <div className="container">
+                    <div className="row align-items-center"> 
+                        <div className="col-md-4">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREX7pmiYsFUwA-opG0WKGaA5qlwdyLCe9uZg&usqp=CAU" className='contact-img' alt="icon"/>
+                        </div>
+                        <div className="col-md-8">
+                        <ul className='list-group'>
+                                            <li className='list-group-item list-group-item-action'>
+                                                Name:<span className='fw-bold'>Jayesh</span>
+                                            </li>
+                                            <li className='list-group-item list-group-item-action'>
+                                                Mobile:<span className='fw-bold'>7894561230</span>
+                                            </li>
+                                            <li className='list-group-item list-group-item-action'>
+                                                Email:<span className='fw-bold'>Jayesh@gmail.com</span>
+                                            </li>
+                                            <li className='list-group-item list-group-item-action'>
+                                                Company:<span className='fw-bold'>Jayesh</span>
+                                            </li>
+                                            <li className='list-group-item list-group-item-action'>
+                                                Group:<span className='fw-bold'>Jayesh</span>
+                                            </li>
+                                            
+                                        </ul>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <Link to={'/contacts/list'} className='btn btn-warning'>Back</Link>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </React.Fragment>
     )
 }
